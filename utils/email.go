ec9a533d5d55aa1e73c2b2e41fefc26c00faad75
Syed Haider Ali Zaidi <haider.zaidiy@gmail.com>
2024-07-28 21:54:58 +0500

    Update email.go

--- utils/email.go
+++ utils/email.go
@@ -10,8 +10,7 @@ import (
 var sendgridAPIKey = os.Getenv("SENDGRID_API_KEY")
 
 func SendVerificationEmail(email, token string) error {
-	//from := mail.NewEmail("AssetCurve", "8info@assetcurve.io")
-	from := mail.NewEmail("AssetCurve", "test@me.io")
+	from := mail.NewEmail("test", "test@me.io")
 	subject := "Email Verification"
 	to := mail.NewEmail("", email)
 	plainTextContent := fmt.Sprintf("Please verify your email using this token: %s", token)
